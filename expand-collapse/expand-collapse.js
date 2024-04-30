62d647cd8d86a590e2ea7d0cc8041cf449ca49c3
Kaloyan Georgiev <kokicha.gg@gmail.com>
2024-04-30 14:49:13 +0300

    feat: transition

--- expand-collapse/expand-collapse.js
+++ expand-collapse/expand-collapse.js
@@ -1,21 +1,13 @@
-// alert('hi')
-
 /**
  * @param {Event} event - The event object
  */
 function openBonusInfo(event) {
-
-    const divExpandCollapse = event.target.parentElement.parentElement.parentElement
-
+    const divExpandCollapse = event.target.parentElement.parentElement;
     const showBonusInfoButton = divExpandCollapse.querySelector('#show-bonus-info');
-    const bonusInfo = divExpandCollapse.querySelector('.bonus-info')
-    // const content = document.querySelector('.content')
-    // const expandCollapseBtn = document.querySelector('.expand-collapse-bgn')
+    const bonusInfo = divExpandCollapse.querySelector('.bonus-info');
 
     showBonusInfoButton.classList.toggle('rotated');
-    bonusInfo.classList.toggle('display-block')
-
-    // content.style.height = "unset";
-    // expandCollapseBtn.style.height = "unset";
-
+    bonusInfo.style.maxHeight = bonusInfo.style.maxHeight
+        ? null
+        : bonusInfo.scrollHeight + "px";
 }

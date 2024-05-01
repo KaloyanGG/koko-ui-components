a0bfcc5a4f9fcca639b9a989c0fd2660d2e586e7
Kaloyan Georgiev <kokicha.gg@gmail.com>
2024-05-01 10:40:09 +0300

    feat: border on focus

--- autocomplete-dropdown/autocomplete-dropdown.js
+++ autocomplete-dropdown/autocomplete-dropdown.js
@@ -1,12 +1,16 @@
 
 
 const input = document.querySelector('#autocomplete-dropdown-input');
+const label = document.querySelector('#autocomplete-dropdown-label')
 const ul = document.querySelector('#item-list');
 
 input.onfocus = () => {
     ul.style.display = 'block';
+    label.classList.toggle('label-for-input-focused');
 }
 
 input.onblur = () => {
     ul.style.display = 'none';
+    label.classList.toggle('label-for-input-focused');
+
 }

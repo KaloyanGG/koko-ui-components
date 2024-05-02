9ff288c54efe299211827aeda7dccd140742825e
Kaloyan Georgiev <kokicha.gg@gmail.com>
2024-05-02 10:59:56 +0300

    refactor: scroll and fix borders

--- autocomplete-dropdown/autocomplete-dropdown.js
+++ autocomplete-dropdown/autocomplete-dropdown.js
@@ -4,13 +4,22 @@ const input = document.querySelector('#autocomplete-dropdown-input');
 const label = document.querySelector('#autocomplete-dropdown-label')
 const ul = document.querySelector('#item-list');
 
+
+const inputWrapper = document.querySelector('.input-wrapper');
+
 input.onfocus = () => {
     ul.style.display = 'block';
     label.classList.toggle('label-for-input-focused');
+    inputWrapper.classList.toggle('focused');
 }
+
 
 input.onblur = () => {
     ul.style.display = 'none';
     label.classList.toggle('label-for-input-focused');
+    inputWrapper.classList.toggle('focused');
+}
 
+inputWrapper.onclick = () => {
+    input.focus();
 }

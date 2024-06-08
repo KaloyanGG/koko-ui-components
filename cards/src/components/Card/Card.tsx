551d8b8abb18b125718faae5450b664ab6daac00
Kaloyan Georgiev <kokicha.gg@gmail.com>
2024-06-08 19:00:22 +0300

    [cards] fix: lagging transition fixed

--- cards/src/components/Card/Card.tsx
+++ cards/src/components/Card/Card.tsx
@@ -7,36 +7,38 @@ type CardProps = {
 export default function Card({ product }: CardProps): JSX.Element {
     return (
         <div className="card">
-            <div className="front">
-                <img src={product.image}
-                    alt="" className="image" />
-                <p className="name">
-                    {product.name}
-                </p>
-            </div>
-            <div className="back">
-                <div>Id</div>
-                <div>{product.id}</div>
-                <div>Price</div>
-                <div className="price">
-                    <span className="regular-price">
-                        {product.regular_price}
-                    </span>
-                    &nbsp;
-                    <span className="sale-price">
-                        {product.sale_price}
-                    </span>
+            <div className="card-inner">
+                <div className="front">
+                    <img src={product.image}
+                        alt="" className="image" />
+                    <div className="name">
+                        {product.name}
+                    </div>
                 </div>
-                <div>Brand</div>
-                <div>{product.brand_name}</div>
-                <div>Pack</div>
-                <div>{product.pack}</div>
-                <div>URL</div>
-                <div>
-                    <a href={product.url}
-                        target="_blank" rel="noreferrer">
-                        {product.url}
-                    </a>
+                <div className="back">
+                    <div>Id</div>
+                    <div>{product.id}</div>
+                    <div>Price</div>
+                    <div className="price">
+                        <span className="regular-price">
+                            {product.regular_price}
+                        </span>
+                        &nbsp;
+                        <span className="sale-price">
+                            {product.sale_price}
+                        </span>
+                    </div>
+                    <div>Brand</div>
+                    <div>{product.brand_name}</div>
+                    <div>Pack</div>
+                    <div>{product.pack}</div>
+                    <div>URL</div>
+                    <div>
+                        <a href={product.url}
+                            target="_blank" rel="noreferrer">
+                            {product.url}
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
